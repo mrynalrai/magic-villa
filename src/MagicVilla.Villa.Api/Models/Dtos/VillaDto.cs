7c98507e81da5734c499e5b95db7d8ca2399f7fb
Mrinal Rai <mrynalrai@gmail.com>
2024-10-26 11:02:23 -0400

    dev: modelState validation

--- src/MagicVilla.Villa.Api/Models/Dtos/VillaDto.cs
+++ src/MagicVilla.Villa.Api/Models/Dtos/VillaDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MagicVilla.Villa.Api.Models.Dtos
 {
     public class VillaDto
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public required string Name { get; set; }
     }
 }
