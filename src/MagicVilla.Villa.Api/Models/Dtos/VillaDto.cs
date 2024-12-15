c35bbb3dfdb4cc79b822c6082b7fc3594145c85e
Mrinal Rai <mrynalrai@gmail.com>
2024-12-15 14:42:38 -0500

    dev: update Villa model

--- src/MagicVilla.Villa.Api/Models/Dtos/VillaDto.cs
+++ src/MagicVilla.Villa.Api/Models/Dtos/VillaDto.cs
@@ -8,7 +8,12 @@ namespace MagicVilla.Villa.Api.Models.Dtos
         [Required]
         [MaxLength(30)]
         public required string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
--- src/MagicVilla.Villa.Api/Models/Villa.cs
+++ src/MagicVilla.Villa.Api/Models/Villa.cs
@@ -4,6 +4,12 @@ namespace MagicVilla.Villa.Api.Models
     {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime Created { get; set; }
     }
 }
