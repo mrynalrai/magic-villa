6a9294918bd18100dbb2bf52e0128bf6d469989a
Mrinal Rai <mrynalrai@gmail.com>
2024-12-24 18:46:24 -0500

    dev: ading create and update dtos

---
+++ src/MagicVilla.Villa.Api/Models/Dtos/VillaCreateDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.Villa.Api.Models.Dtos
+{
+    public class VillaCreateDto
+    {
+        [Required]
+        [MaxLength(30)]
+        public required string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+    }
+}
---
+++ src/MagicVilla.Villa.Api/Models/Dtos/VillaUpdateDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.Villa.Api.Models.Dtos
+{
+    public class VillaUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public required string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        [Required]
+        public int Occupancy { get; set; }
+        [Required]
+        public int Sqft { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+    }
+}
