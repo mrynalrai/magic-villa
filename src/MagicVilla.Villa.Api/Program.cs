263c0023f9c08ae84c702a9bef69712f0a45b63d
Mrinal Rai <mrynalrai@gmail.com>
2024-12-28 18:19:49 -0500

    dev: Return supported versions in response headers

--- src/MagicVilla.Villa.Api/Program.cs
+++ src/MagicVilla.Villa.Api/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddApiVersioning(options => {
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
 });
 builder.Services.AddVersionedApiExplorer(options => {
     options.GroupNameFormat = "'v'VVV";
